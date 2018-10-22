22dec1b790df7a64840c31c9b892682854c65d18
Dzmitry Bosak <Dzmitry_Bosak@epam.com>
2018-10-22 15:36:11 +0300

    Unnecessary tests removed

--- NewsTests/NewsTests.swift
+++ NewsTests/NewsTests.swift
@@ -11,62 +11,12 @@ import XCTest
 
 class NewsTests: XCTestCase {
     
-    var sessionUnderTest: URLSession!
-    
     override func setUp() {
         super.setUp()
-        sessionUnderTest = URLSession(configuration: URLSessionConfiguration.default)
     }
 
     override func tearDown() {
-        sessionUnderTest = nil
         super.tearDown()
-    }
-    
-    // Asynchronous test: success fast, failure slow
-    func testValidCallToAPIGetsHTTPStatusCode200() {
-        // given
-        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=97d1c467a9584168a55584355bc778a8")
-        let promise = expectation(description: "Status code: 200")
-        
-        // when
-        let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
-            // then
-            if let error = error {
-                XCTFail("Error: \(error.localizedDescription)")
-                return
-            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-                if statusCode == 200 {
-                    promise.fulfill()
-                } else {
-                    XCTFail("Status code: \(statusCode)")
-                }
-            }
-        }
-        dataTask.resume()
-        waitForExpectations(timeout: 5, handler: nil)
-    }
-    
-    // Asynchronous test: faster fail
-    func testCallToAPICompletes() {
-        // given
-        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=97d1c467a9584168a55584355bc778a8")
-        let promise = expectation(description: "Completion handler invoked")
-        var statusCode: Int?
-        var responseError: Error?
-        
-        // when
-        let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
-            statusCode = (response as? HTTPURLResponse)?.statusCode
-            responseError = error
-            promise.fulfill()
-        }
-        dataTask.resume()
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        // then
-        XCTAssertNil(responseError)
-        XCTAssertEqual(statusCode, 200)
     }
 
 }
